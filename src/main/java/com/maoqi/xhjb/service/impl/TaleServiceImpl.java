93835db6fc00bed63b8a50471c4b49c826a2a77e
zwluo <luozhiweideyou@163.com>
2021-01-28 14:17:28 +0800

    添加兴化市卷本说明，以及优化代码 -- by lzw

--- src/main/java/com/maoqi/xhjb/service/impl/TaleServiceImpl.java
+++ src/main/java/com/maoqi/xhjb/service/impl/TaleServiceImpl.java
@@ -42,11 +42,12 @@ public class TaleServiceImpl implements TaleService {
 
     @Override
     public void saveTales() {
-        List<Tale> list = null;
+        List<Tale> list;
         try {
             list = getTalesFromTxt();
             list.addAll(getBalladFromTxt());
             list.addAll(getProverbFromTxt());
+            list.addAll(getDescribe());
             taleDao.saveTales(list);
         } catch (IOException e) {
             e.printStackTrace();
@@ -70,7 +71,7 @@ public class TaleServiceImpl implements TaleService {
 
         Tale tale = new Tale();
 
-        String line = null;
+        String line;
         while ((line = reader.readLine()) != null) {
             // 故事类别
             if("神话".equals(line) || "传说".equals(line) || "故事".equals(line)) {
@@ -102,6 +103,10 @@ public class TaleServiceImpl implements TaleService {
                         tale.setCreatedate(new Date());
                         tale.setCreateby("system");
 
+                        // 替换掉内容里面的空格
+                        if (tale.getContent() != null) {
+                            tale.setContent(tale.getContent().replaceAll(" ", ""));
+                        }
                         list.add(tale);
                         tale = new Tale();
                     } else {
@@ -109,10 +114,9 @@ public class TaleServiceImpl implements TaleService {
                     }
                 } else {        // 故事内容
                     if(StringUtils.isBlank(tale.getContent())) {
-                        // 替换掉内容里面的空格
-                        tale.setContent(line.replaceAll(" ", ""));
+                        tale.setContent("　　" + line);
                     } else {
-                        tale.setContent(tale.getContent() + "\n" + line);
+                        tale.setContent(tale.getContent() + "\n" + "　　" + line);
                     }
                 }
             }
@@ -121,17 +125,19 @@ public class TaleServiceImpl implements TaleService {
         return list;
     }
 
+    /**
+     * 歌谣
+     * @return 歌谣内容
+     * @throws IOException 异常
+     */
     public List<Tale> getBalladFromTxt() throws IOException {
         File file = new File("d:" + File.separator + "ballad.txt");
-        BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file));
-        inputStream.available();
-
         BufferedReader reader = new BufferedReader(new FileReader(file));
 
-        String line = null;
+        String line;
 
         // 故事类型
-        String type = "";
+        String type = "歌谣";
 
         // 上一行是否是故事类型
         boolean lastIsType = false;
@@ -171,10 +177,14 @@ public class TaleServiceImpl implements TaleService {
                         tale.setNarrator(line.substring(line.indexOf("：") + 1).replaceAll("\\s+", " "));
 
                         // 设置类型
-                        tale.setType("歌谣");
+                        tale.setType(type);
                         tale.setCreatedate(new Date());
                         tale.setCreateby("system");
 
+                        // 替换掉内容里面的空格
+                        if (tale.getContent() != null) {
+                            tale.setContent(tale.getContent().replaceAll(" ", ""));
+                        }
                         list.add(tale);
                         tale = new Tale();
                     } else {
@@ -182,8 +192,7 @@ public class TaleServiceImpl implements TaleService {
                     }
                 } else {        // 故事内容
                     if(StringUtils.isBlank(tale.getContent())) {
-                        // 替换掉内容里面的空格
-                        tale.setContent(line.replaceAll(" ", ""));
+                        tale.setContent(line);
                     } else {
                         tale.setContent(tale.getContent() + "\n" + line);
                     }
@@ -193,11 +202,16 @@ public class TaleServiceImpl implements TaleService {
         return list;
     }
 
+    /**
+     * 谚语
+     * @return 谚语
+     * @throws IOException 异常
+     */
     public List<Tale> getProverbFromTxt() throws IOException {
         File file = new File("d:" + File.separator + "proverb.txt");
         BufferedReader reader = new BufferedReader(new FileReader(file));
 
-        String line = null;
+        String line;
 
         // 故事类型
         String type = "谚语";
@@ -223,16 +237,102 @@ public class TaleServiceImpl implements TaleService {
                 }
 
                 if(StringUtils.isBlank(tale.getContent())) {
-                    // 替换掉内容里面的空格
-                    tale.setContent(line.replaceAll(" ", ""));
+                    tale.setContent(line);
                 } else {
                     tale.setContent(tale.getContent() + "\n" + line);
                 }
             }
         }
         if (tale != null) {
+            // 替换掉内容里面的空格
+            if (tale.getContent() != null) {
+                tale.setContent(tale.getContent().replaceAll(" ", ""));
+            }
             list.add(tale);
         }
+
+        return list;
+    }
+
+    /**
+     * 描述
+     * @return 描述
+     */
+    public List<Tale> getDescribe() {
+        List<Tale> list = new ArrayList<>();
+        Date date = new Date();
+        Tale tale = new Tale();
+        tale.setType("关于本书");
+        tale.setTitle("编后");
+        tale.setContent("　　为了进一步开创民间文学的新局面，保存人民的口头文学财富，在省、扬州市有关部门和我市市委、市人大、市政府、" +
+                "市政协领导同志的关心支持下，经过三年的搜集整理工作，《兴化市卷本》现在和读者见面了。省“三民”办公室唐雨奇、" +
+                "陈为史和我市刁俊、毕明、仲鲁连、陈康、潘少卿等同志，为《兴化市卷本》的编辑做了大量的工作，在此谨致谢忱。\n" +
+                "　　编者\n" + "　　一九九三年十二月六日");
+        tale.setCreatedate(date);
+        tale.setCreateby("system");
+        list.add(tale);
+
+        tale = new Tale();
+        tale.setType("关于本书");
+        tale.setTitle("各乡镇入选作品");
+        tale.setContent("单　位　民间故事　歌　谣　谚　语\n" +
+                "戴南镇　　11　　　　16　　237\n" +
+                "张郭乡　　2　　　　　6\n" +
+                "唐刘乡　　11　　　　11\n" +
+                "顾庄乡　　　　　　　1\n" +
+                "周庄镇　　1　　　　　6\n" +
+                "边城镇　　3　　　　　　　　11\n" +
+                "茅山镇　　1　　　　　2　　　9\n" +
+                "陈堡乡　　1\n" +
+                "竹泓镇　　9　　　　　2\n" +
+                "沈伦乡　　2　　　　　1\n" +
+                "林湖乡　　3　　　　　2\n" +
+                "垛田乡　　1　　　　　1\n" +
+                "安丰镇　　9　　　　　4\n" +
+                "新垛乡　　　　　　　　　　　4\n" +
+                "中圩乡　　1　　　　　4　　　21\n" +
+                "钓鱼乡　　3\n" +
+                "下圩乡　　1\n" +
+                "大邹镇　　3　　　　　　　　20\n" +
+                "海河乡　　2\n" +
+                "海南乡　　1\n" +
+                "昌荣镇　　5\n" +
+                "大垛镇　　　　　　　2\n" +
+                "荻垛乡　　　　　　　3\n" +
+                "陶庄乡　　2\n" +
+                "沙沟镇　　3　　　　　2\n" +
+                "舜生镇　　　　　　　2\n" +
+                "周奋乡　　6　　　　　1\n" +
+                "中堡镇　　1\n" +
+                "戴窑镇　　2　　　　　　　　50\n" +
+                "合塔乡　　1　　　　　1\n" +
+                "舍陈镇\n" +
+                "徐扬乡　　　　　　　1　　　48\n" +
+                "西鲍乡　　　　　　　　　　　8\n" +
+                "临城乡　　　　　　　1　　　10\n" +
+                "东鲍乡\n" +
+                "荡朱乡　　7　　　　　6\n" +
+                "东潭乡　　1　　　　　2\n" +
+                "昭阳镇　　2\n" +
+                "市　直　　95　　　　　90　　322\n" +
+                "其　他　　12　　　　　6　　　8\n" +
+                "合　计　　202　　　　173　　748");
+        tale.setCreatedate(date);
+        tale.setCreateby("system");
+        list.add(tale);
+
+        tale = new Tale();
+        tale.setType("关于本书");
+        tale.setTitle("网页版说明");
+        tale.setContent("　　这本书出版日期是1993年10月，当时兴化还属于扬州，由兴化市民间文学集成办公室历时3年采编完成。" +
+                "很感谢前辈们的先见之明和辛勤付出，才让我们有机会读到这些精彩的民间故事和人生哲理，里面有很多兴化方言，读来倍感亲切。" +
+                "童话故事不是安徒生的专利，我们的先辈们讲故事也是很有一手的。\n" +
+                "　　里面的故事并不是很全，只希望能有人继续采集这些民间故事，因为口口相传的故事都会随着时间慢慢消逝，" +
+                "先辈们留下的瑰宝不能在我们这代葬送，我能做的也就这些了。\n" +
+                "　　二零一七年九月九日");
+        tale.setCreatedate(date);
+        tale.setCreateby("system");
+        list.add(tale);
 
         return list;
     }
